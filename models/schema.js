db8e803fa7c05d9f9208cf4ab9092f00075928cb
Ruchi <ruchiar197@gmail.com>
2025-04-28 13:25:20 +0530

    Added signin/signup feature

--- models/schema.js
+++ models/schema.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";  
+import mongoose from "mongoose";
 
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   note: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User model
 });
 
 export const Note = mongoose.model("Note", noteSchema);
